c44938196531b69bb114d8af7d02e89537105fbb
JParras02 <99205424+JParras02@users.noreply.github.com>
2023-03-03 18:24:51 +0100

    Initial commit

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Product, products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+})
+export class ProductListComponent {
+  products = [...products];
+
+  share() {
+    window.alert('El producto ha sido compartido');
+  }
+
+  onNotify() {
+    window.alert('Seras notificado cuando haya stock');
+  }
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
